560eb43daac8450a0a02640f55996185ce42d5b2
Amir <mehdizadeh.amir11@gmail.com>
2022-10-23 22:41:13 +0330

    Array Method Ex Added(create keyed object)

---
+++ Array Methods Exercises/CreateKeyedObject.js
@@ -0,0 +1,17 @@
+let users = [
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+  { id: "pete", name: "Pete Peterson", age: 31 },
+];
+
+//this method is based on my knowledge
+function groupById(array) {
+  const newObj = array.reduce((acc, item, index, array) => {
+    acc[item.id] = item;
+    return acc;
+  }, {});
+  return newObj
+}
+
+let usersById = groupById(users);
+console.log(usersById)
