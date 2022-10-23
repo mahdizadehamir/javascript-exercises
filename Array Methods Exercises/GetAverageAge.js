b1648153217f184fb3251aaa89dbf3a4e64cc2a0
Amir <mehdizadeh.amir11@gmail.com>
2022-10-23 21:11:47 +0330

    Array Method Ex added(revisited)

--- Array Methods Exercises/GetAverageAge.js
+++ Array Methods Exercises/GetAverageAge.js
@@ -1,13 +1,18 @@
 let john = { name: "John", age: 25 };
 let pete = { name: "Pete", age: 30 };
 let mary = { name: "Mary", age: 29 };
-
 let arr = [john, pete, mary];
+//this method is based on my knowledge
+// function getAverageAge(array) {
+//   let sum = 0;
+//   for (let dude of array) {
+//     sum += dude.age;
+//   }
+//   return sum / array.length
+// }
+
+//use reduce method
 function getAverageAge(array) {
-  let sum = 0;
-  for (let dude of array) {
-    sum += dude.age;
-  }
-  return sum / array.length
+  return array.reduce((prev, item) => prev + item.age, 0) / array.length;
 }
-alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+alert(getAverageAge(arr)); // (25 + 30 + 29) / 3 = 28
