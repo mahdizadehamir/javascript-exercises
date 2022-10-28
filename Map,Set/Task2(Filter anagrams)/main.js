195dbaa551a1ccf7ab88f37de3a900bda8e97ac1
unknown <mehdizadeh.amir11@gmail.com>
2022-10-28 22:24:56 +0330

    map,set anagrams revisited

--- Map,Set/Task2(Filter anagrams)/main.js
+++ Map,Set/Task2(Filter anagrams)/main.js
@@ -1,13 +1,7 @@
 let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
 function aclean(array) {
   //this code base on my knowledge
-  let seti = new Set(array);
-  let newseti = new Set();
-  seti.forEach((value,value2,set) => {
-    newseti.add(value.toLowerCase().split('').sort().join(''));
-    return newseti 
-  })
-  console.log(newseti)
+let  
 }
 
 // alert(aclean(arr)); // "nap,teachers,ear" or "PAN,cheaters,era"
