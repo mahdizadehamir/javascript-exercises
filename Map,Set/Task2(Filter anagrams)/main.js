c52d1f365e627bf3b89ef1747fb62d24099bdc76
unknown <mehdizadeh.amir11@gmail.com>
2022-10-28 22:44:34 +0330

    finnaly anagrams filtered

--- Map,Set/Task2(Filter anagrams)/main.js
+++ Map,Set/Task2(Filter anagrams)/main.js
@@ -1,8 +1,14 @@
 let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
 function aclean(array) {
-  //this code base on my knowledge
-let  
+  //this code is not based my knowledge
+const mapper = new Map();
+for(let word of array){
+  let sorted = word.toLowerCase().split('').sort().join('');
+  mapper.set(sorted,word)
+}
+return Array.from(mapper.values())
+  
 }
 
 // alert(aclean(arr)); // "nap,teachers,ear" or "PAN,cheaters,era"
-aclean(arr);
+console.log(aclean(arr));
